bb672560986863f909f9ede4fe37d805aa344e7b
ShinSeoY <tjdudtls312@gmail.com>
2025-10-02 16:24:15 +0900

    - vue config 수정

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,8 @@
-const { defineConfig } = require('@vue/cli-service');
+const {defineConfig} = require('@vue/cli-service');
 module.exports = defineConfig({
+    publicPath: '/', // 정적 경로는 루트 기준
     transpileDependencies: true,
-});
-
-module.exports = {
     devServer: {
-        port: 5173,
+        port: 5173, // 개발 서버 포트
     },
-};
+});
